4ab979437ea7d949ac6099983b23f15abdfbb3ad
Jelle Kralt <jelle@jellekralt.nl>
2019-06-27 16:31:58 +0200

    feature: refactor to casper.aero

--- src/runways.js
+++ src/runways.js
@@ -1,55 +1,101 @@
-const osmosis = require('osmosis');
+const https = require('https');
 
 const runwayInfo = {
-    'polderbaan': { 
-        name: 'Polderbaan', 
-        code: '18R - 36L' 
-    },
-    'zwanenburgbaan': { 
-        name: 'Zwanenburgbaan', 
-        code: '18C - 36C' 
-    },
-    'buitenveldertbaan': { 
-        name: 'Buitenveldertbaan', 
-        code: '09 - 27' 
-    },
-    'kaagbaan': { 
-        name: 'Kaagbaan', 
-        code: '06 - 24' 
-    },
-    'aalsmeerbaan': { 
-        name: 'Aalsmeerbaan', 
-        code: '18L - 36R' 
-    },
-    'oostbaan': { 
-        name: 'Oostbaan', 
-        code: '04 - 22' 
-    }
-}
+  '18R-36L': {
+    name: 'Polderbaan',
+    code: '18R - 36L'
+  },
+  '18C-36C': {
+    name: 'Zwanenburgbaan',
+    code: '18C - 36C'
+  },
+  '09-27': {
+    name: 'Buitenveldertbaan',
+    code: '09 - 27'
+  },
+  '06-24': {
+    name: 'Kaagbaan',
+    code: '06 - 24'
+  },
+  '18L-36R': {
+    name: 'Aalsmeerbaan',
+    code: '18L - 36R'
+  },
+  '04-22': {
+    name: 'Oostbaan',
+    code: '04 - 22'
+  }
+};
+const RUNWAY_URL = 'https://flighttracking.casper.aero/ams/runway/use';
+const RUNWAY_URL_PARAMS = '?initial=true';
 
 module.exports = function runways() {
-    let total = [];
+  return new Promise((resolve, reject) => {
+    const now = new Date().getTime();
 
-    return new Promise((resolve, reject) => {
-        osmosis
-            .get('https://www.lvnl.nl/nl/airtraffic')
-            .find('#runwayVisual li')
-            .set({
-                'name': '@id',
-                'type': '@class'
-            })
-            .data((listing) => {
-                let runway = Object.assign({}, listing, runwayInfo[listing.name]);
+    const url =
+      RUNWAY_URL +
+      RUNWAY_URL_PARAMS +
+      `&from=${now}&upto=${now}&status[0]=CALCULATED`;
 
-                runway.type = listing.type !== '' ? listing.type : null;
-                runway.open = !!listing.type;
+    https
+      .get(url, resp => {
+        let data = '';
 
-                total.push(runway);
+        // A chunk of data has been recieved.
+        resp.on('data', chunk => {
+          data += chunk;
+        });
 
-                return runway;
-            })
-            .done(() => {
-                resolve(total);
-            });
-    });
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          resolve(parseData(JSON.parse(data)));
+        });
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+};
+
+function parseData(data) {
+  let landing = parseRunways(data[0], 'landing');
+  let takeoff = parseRunways(data[0], 'takeoff');  
+  
+  return mapObject(runwayInfo, (runway) => {
+    let [runwayKey, runwayData] = runway;
+    const isLanding = landing.some(r=> runwayKey.split('-').includes(r));
+    const isTakeoff = takeoff.some(r=> runwayKey.split('-').includes(r));
+
+    runwayData.open = false;
+    runwayData.type = null;
+
+    if (isLanding) {
+      runwayData.type = 'landing';
+      runwayData.open = true;
+    }
+    if (isTakeoff) {
+      runwayData.type = 'takeoff';
+      runwayData.open = true;
+    }
+    
+    return runway;
+  });
+}
+
+function parseRunways(data, type) {
+  return Object.keys(data).map((key) => {    
+    if (key.substr(0,7) === type) {
+      return data[key];
+    }
+  }).filter(runway => runway);
+}
+
+function mapObject(input, cb) {
+  return Object.entries(input)
+    .map(cb)
+    .reduce((acc, item) => {
+      acc[item[0]] = item[1];
+      return acc;
+    }, {});
 }
---
+++ test/runways.spec.js
@@ -0,0 +1,19 @@
+const test = require('tape');
+const schipholRunways = require('../dist/runways');
+
+test('runways(): should pass runways', async (t) => {
+  let runways = await schipholRunways();
+  
+  console.log('data', runways);
+  
+  console.log('');
+  console.log('');
+  console.log('----------------');
+  console.log('');
+  
+  
+  
+  
+
+  t.end();
+});
